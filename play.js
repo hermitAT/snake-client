62147a2230ca72c73875d113200814b717d18e04
Adam Thorne <ahthornev2@gmail.com>
2020-11-04 20:50:30 +0000

    added functionality to exit from the gameusing ctrl+c

--- play.js
+++ play.js
@@ -2,3 +2,27 @@ const connect = require('./client');
 
 console.log('Connecting ...');
 connect();
+
+/**
+ * Setup User Interface
+ * Specifically, so that we can handle user input via stdin
+ */
+const handleUserInterfaces = (data) => {
+  if (data === "\u0003") {
+    process.exit();
+  }
+};
+
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', (handleUserInterfaces));
+  return stdin;
+};
+
+
+
+setupInput();
