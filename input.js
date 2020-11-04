e42a58be963f7e2f56ae141a4219902d5021fe6e
Adam Thorne <ahthornev2@gmail.com>
2020-11-04 21:18:22 +0000

    added functionality to send movement inputs to the server based on the connection

--- input.js
+++ input.js
@@ -2,19 +2,36 @@
  * Setup User Interface
  * Specifically, so that we can handle user input via stdin
  */
-const handleUserInterfaces = (data) => {
-  if (data === "\u0003") {
-    process.exit();
-  }
-};
+let connection;
 
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
+  stdin.on('data', (data) => {
+    if (data === "w") {
+      connection.write("Move: up");
+    }
+    if (data === "a") {
+      connection.write("Move: left");
+    }
+    if (data === "s") {
+      connection.write("Move: down");
+    }
+    if (data === "d") {
+      connection.write("Move: right");
+    }
+  });
   stdin.on('data', (handleUserInterfaces));
   return stdin;
+};
+
+const handleUserInterfaces = (data) => {
+  if (data === "\u0003") {
+    process.exit();
+  }
 };
 
 module.exports = setupInput;
